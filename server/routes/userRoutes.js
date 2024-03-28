a3d9111f72bfb5c59716920551a65ebe7e2442ac
chikadike <dikechika87@gmail.com>
2024-03-29 00:52:53 +0100

    added userschema

---
+++ server/routes/userRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const userController = require('../controllers/userController');
+
+// Route to get all users
+router.get('/users', userController.getAllUsers);
+
+// Route to create a new user
+router.post('/users', userController.createUser);
+
+// Export the router
+module.exports = router;
